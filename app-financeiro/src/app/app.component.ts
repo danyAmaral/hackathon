21a5a5a98687773c34920b2dd47dc2d89170e951
higorrebell0 <higorrebello@gmail.com>
2019-10-30 16:30:53 -0300

    aplicação do intl para tradução do devextreme

--- app-financeiro/src/app/app.component.ts
+++ app-financeiro/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import * as ptMessages from 'devextreme/localization/messages/pt.json';
+import { loadMessages, locale } from 'devextreme/localization';
+import config from 'devextreme/core/config';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor() {
+    loadMessages(ptMessages);
+    locale(navigator.language);
+    config({ defaultCurrency: 'BRL' });
+  }
   title = 'app-financeiro';
 }
--- app-financeiro/src/app/app.module.ts
+++ app-financeiro/src/app/app.module.ts
@@ -2,25 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { NgxCurrencyModule } from "ngx-currency";
-
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { CardsComponent } from './dashboard/cards/cards.component';
 import { GraficoCrescimentoComponent } from './dashboard/grafico-crescimento/grafico-crescimento.component';
 import { GraficoSetorComponent } from './dashboard/grafico-setor/grafico-setor.component';
 import { ListagemComponent } from './dashboard/listagem/listagem.component';
 import { PropostaComponent } from './proposta/proposta.component';
-
 import { RouterModule } from '@angular/router';
 import { ROUTES } from './app.routes';
 import { TopoComponent } from './topo/topo.component';
 import { RodapeComponent } from './rodape/rodape.component';
 import { HttpClientModule } from '@angular/common/http';
-
 import { PropostaService } from './proposta.service';
-
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import {
@@ -31,9 +25,11 @@ import {
   DxTemplateModule,
   DxChartModule,
   DxButtonModule,
-  DxDataGridModule
+  DxDataGridModule,
   DxPieChartModule
 } from 'devextreme-angular';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
