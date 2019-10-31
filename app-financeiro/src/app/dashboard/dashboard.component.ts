0b527df9b0ca6c1059d290f8202cbafdb9c0f1b5
danyAmaral <danyamaral27@gmail.com>
2019-10-30 22:15:09 -0300

    filtro funcionando

--- app-financeiro/src/app/dashboard/dashboard.component.ts
+++ app-financeiro/src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit} from '@angular/core';
 import { PropostaDashboard } from '../shared/proposta.dashboard.model';
 import { PropostaService } from '../proposta.service';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
@@ -11,6 +12,10 @@ export class DashboardComponent implements OnInit {
 
  public propostas: Array<PropostaDashboard>;
  public itensPropostaCache: Array<PropostaDashboard>;
+ public filtro: FormGroup = new FormGroup({
+  'area': new FormControl('Todas'),
+  'status': new FormControl('Todos'),
+});
 
   constructor(private propostaService: PropostaService) { }
 
@@ -22,6 +27,13 @@ export class DashboardComponent implements OnInit {
   }
 
   public filtrar():void{
-    this.itensPropostaCache = this.propostas.filter((item) => {return item.area == 'Recursos Humanos'})
+     let filtroArea = this.filtro.value.area;
+     let filtroStatus = this.filtro.value.status;
+console.log(filtroArea);
+console.log(filtroStatus);
+    this.itensPropostaCache = this.propostas.filter((item) => {
+      return ((filtroArea == 'Todas' || item.area == filtroArea) &&
+              (filtroStatus == 'Todos' || item.status == filtroStatus));
+    })
   }
 }
